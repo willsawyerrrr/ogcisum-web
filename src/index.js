85e19e3b85c53b394f11ee4b3a522eccc144c863
William Sawyer <wmsawyer2609@gmail.com>
2022-09-07 20:23:47 +1000

    Renamed colours CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./css/index.css";
-import "./css/style.css";
+import "./css/colours.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
