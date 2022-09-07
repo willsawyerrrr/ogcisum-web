5f469a8e620df10e395100fe53d664eda58c4cca
William Sawyer <wmsawyer2609@gmail.com>
2022-09-08 02:00:50 +1000

    Implemented sticky header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,28 @@
-function Header() {
-    return (
-        <h1>This is the header component.</h1>
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+import "../css/header.css";
+
+function Header({ back }) {
+    return back ? (
+        <header>
+            <Link to="/">
+                <h1 className="logo">
+                    <FontAwesomeIcon icon={faArrowLeft} /> &nbsp; OgCiSum
+                </h1>
+            </Link>
+
+            <p>Create &amp; Share Samples, Listen in Mobile App!</p>
+        </header >
+    ) : (
+        <header>
+            <Link to="/">
+                <h1 className="logo">OgCiSum</h1>
+            </Link>
+
+            <p>Create &amp; Share Samples, Listen in Mobile App!</p>
+        </header>
     );
 }
 
