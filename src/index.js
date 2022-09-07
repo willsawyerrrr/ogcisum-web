64eff1fa626e5af65b9991034865cc6e90cb634a
William Sawyer <wmsawyer2609@gmail.com>
2022-09-07 20:23:11 +1000

    Added app colours

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./css/index.css";
+import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
