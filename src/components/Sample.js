aa88fe9a78cb08f41f311cdf5dec143816ddc5e9
William Sawyer <wmsawyer2609@gmail.com>
2022-10-01 12:50:33 +1000

    Converted date format

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -3,12 +3,27 @@ import { Link } from 'react-router-dom';
 import "../css/samples.css";
 
 function Sample({ id, datetime, recording_data }) {
+    const months = {
+        0: "January",
+        1: "February",
+        2: "March",
+        3: "April",
+        4: "May",
+        5: "June",
+        6: "July",
+        7: "August",
+        8: "September",
+        9: "October",
+        10: "November",
+        11: "December"
+    }
+
     let date = new Date(datetime);
     let hours = date.getHours();
     hours = hours % 12;
     hours = hours ? hours : 12;
     let time = `${hours}:${date.getMinutes()} ${date.getHours() >= 12 ? "pm" : "am"}`;
-    let dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    let dateString = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 
     return (
         <div className="box sample">
